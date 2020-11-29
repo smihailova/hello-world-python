03c03e20849dc6e84cb0c644c9ebe0163d94c49c
Sofia Mihailova <smihailova@thumbtack.net>
2020-11-29 16:41:16 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/hello/<username>')
+def hello_user(username):
+    return 'Hello %s, Sofia and Gideon!' % username
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
